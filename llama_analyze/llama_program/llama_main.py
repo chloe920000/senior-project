71d92b39b49ee895e2c1c608a22efc8d0bd486d3
JINC419 <asd10933@gmail.com>
2024-08-27 22:05:18 +0800

    output

--- llama_analyze/llama_program/llama_main.py
+++ llama_analyze/llama_program/llama_main.py
@@ -18,7 +18,7 @@ from prompt_generater import *
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # 設置結果資料相對路徑
-result_data_dir = os.path.join(script_dir, '..', 'test_output')
+result_data_dir = os.path.join(script_dir, '..', 'analyze result')
 if not os.path.exists(result_data_dir):
     os.makedirs(result_data_dir)
     
