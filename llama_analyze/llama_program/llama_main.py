748b50a738f520cf8b8e26f44b21322a0b7993bf
yayyaysu <ian1231231236@gmail.com>
2024-08-30 14:21:13 +0800

    requirement修改

--- llama_analyze/llama_program/llama_main.py
+++ llama_analyze/llama_program/llama_main.py
@@ -69,8 +69,8 @@ def get_some_stock_ids(begin, end):
     stock_ids = [str(item['stockID']) for item in response.data]
     return stock_ids
 
-# dates = ['2020-11-13', '2022-04-19', '2022-09-07', '2023-06-07']
-dates = ['2021-12-30']
+# dates = ['2021-12-30', '2020-12-31', '2019-12-31', '2018-12-28']
+dates = ['2018-12-28']
 # 獲取要分析的所有股票的 `stock_id` 列表
 # stock_ids = get_all_stock_ids()
 stock_ids = get_some_stock_ids(2000,3000) # 只分析部分stock_id
